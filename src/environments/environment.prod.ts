e71fc88a3a6f204dd22d93888dbc5df26b647fbf
karthikeyan-1234 <mail2karthikkn@gmail.com>
2025-12-30 17:06:04 +0530

    Updated the payment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'http://kong-proxy.local/api',
-  paymentBaseUrl: 'https://localhost:7169/api',
+  paymentBaseUrl: 'http://kong-proxy.local/api',
   swaggerUrl: 'http://kong-proxy.local/swagger/index.html',
   stripePublishableKey: 'pk_test_51NNIIMSHXTQ38KZZ9xN8WOddbu2pYwPvqPUMe3PYTAMxv8wfhPUzk3kVsJQcwNiz7ezLz3SSIRWpZTEWDr297eRB00K3QGopZV'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'https://localhost:7216/api',
-  paymentBaseUrl: 'https://localhost:7169/api',
+  paymentBaseUrl: 'https://localhost:7216/api',
   swaggerUrl: 'https://localhost:7216/swagger/index.html',
   stripePublishableKey: 'pk_test_51NNIIMSHXTQ38KZZ9xN8WOddbu2pYwPvqPUMe3PYTAMxv8wfhPUzk3kVsJQcwNiz7ezLz3SSIRWpZTEWDr297eRB00K3QGopZV'
 };
