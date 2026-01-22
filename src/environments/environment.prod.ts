244a836ef4721db7d27f1b9e3fc09c1e15805cb4
karthikeyan-1234 <mail2karthikkn@gmail.com>
2026-01-22 13:32:32 +0530

    modified keycloak url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   production: true,
   apiBaseUrl: 'http://kong-proxy.local/api',
   paymentBaseUrl: 'http://kong-proxy.local/api',
-  keycloakUrl: 'http://keycloak.local/',
+  keycloakUrl: 'http://keycloak.local',
   swaggerUrl: 'http://kong-proxy.local/swagger/index.html',
   stripePublishableKey: 'pk_test_51NNIIMSHXTQ38KZZ9xN8WOddbu2pYwPvqPUMe3PYTAMxv8wfhPUzk3kVsJQcwNiz7ezLz3SSIRWpZTEWDr297eRB00K3QGopZV'
 };
