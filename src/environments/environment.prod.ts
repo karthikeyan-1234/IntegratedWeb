acf90d09c2a3e18749860d5471a4c81755be2a68
karthikeyan-1234 <mail2karthikkn@gmail.com>
2026-01-27 15:11:30 +0530

    Modified environ.prod.ts to https version of kong

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://kong-proxy.local/api',
-  paymentBaseUrl: 'http://kong-proxy.local/api',
-  keycloakUrl: 'http://keycloak.local',
-  swaggerUrl: 'http://kong-proxy.local/swagger/index.html',
+  apiBaseUrl: 'https://kong-proxy.local/api',
+  paymentBaseUrl: 'https://kong-proxy.local/api',
+  keycloakUrl: 'https://keycloak.local',
+  swaggerUrl: 'https://kong-proxy.local/swagger/index.html',
   stripePublishableKey: 'pk_test_51NNIIMSHXTQ38KZZ9xN8WOddbu2pYwPvqPUMe3PYTAMxv8wfhPUzk3kVsJQcwNiz7ezLz3SSIRWpZTEWDr297eRB00K3QGopZV'
 };
